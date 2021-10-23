24a9d6bbc3bca032c1bc86d01c86d45d542960eb
clarkeysg <clarkeysg@gmail.com>
2021-10-23 13:29:20 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//Function Decs
+
+//Script Calls
+var vg_1 = "overallHappinessRank.vega"; vegaEmbed("#ranking_bar_chart", vg_1).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_2 = "socialSupportHealthyLife.vega"; vegaEmbed("#healthy_life_scatter_plot", vg_2).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_8 = "socialSupportHappinessScore.vega"; vegaEmbed("#social_support_scatter_plot", vg_8).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_3 = "worldVegaWorking.vega"; vegaEmbed("#score_world_map", vg_3).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_4 = "corruptionMap.vega"; vegaEmbed("#corruption_world_map", vg_4).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_5 = "smallMultiple0.vega"; vegaEmbed("#sm0", vg_5).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_6 = "smallMultiple1.vega"; vegaEmbed("#sm1", vg_6).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_7 = "smallMultiple2.vega"; vegaEmbed("#sm2", vg_7).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
